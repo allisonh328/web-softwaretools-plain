63c78a726e93c5875621e405d7afe31972105d87
Luming-Liu <kr20826@bristol.ac.uk>
2021-08-01 06:53:56 +0800

    Initially complete register feature.

---
+++ scripts/registerPage.js
@@ -0,0 +1,111 @@
+const input = document.querySelector("#exampleInputPhone");
+window.intlTelInput(input, {});
+
+document.querySelector(".iti--allow-dropdown").className += " col-sm-12";
+document.querySelector(".iti__selected-flag").setAttribute("style", "padding-left: 25px");
+
+$(function () {
+    $('#form').on('submit', function (e) {
+        e.preventDefault();
+
+        if (userCheck() && pwdCheck()) {
+            const dataArray = [];
+            const dataJson = {
+                "username": $("#exampleInputUsername").val(),
+                "firstName": $("#exampleFirstName").val(),
+                "lastName": $("#exampleLastName").val(),
+                "email": $("#exampleInputEmail").val(),
+                "password": $("#exampleInputPassword").val(),
+                "phone": $("#exampleInputPhone").val()
+            };
+            dataArray.push(dataJson);
+
+            $.ajax({
+                url: "https://petstore.swagger.io/v2/user/createWithArray",
+                type: "POST",
+                async: false,
+                cache: false,
+                dataType: "json",
+                contentType: "application/json",
+                traditional: true,
+                data: JSON.stringify(dataArray),
+                success: function (data) {
+                    console.log(data);
+                    alert("You have successfully registered!");
+                    window.location.replace("login.html");
+                },
+                error: function (error) {
+                    console.log(error.responseJSON);
+                    alert("Registration failed!")
+                }
+            });
+        }
+    });
+});
+
+function pwdCheck() {
+    if (!$("#exampleInputPassword").val()){
+        $("#error2").remove();
+        $("#pwd_check").append(
+            '<ul class="errorlist" id="error2"><li style="color: red; font-size: 20%">' +
+            'Password can not be empty.</li></ul>'
+        );
+        return false;
+    }
+    else if ($("#exampleInputPassword").val()!==$("#exampleRepeatPassword").val()){
+        $("#error2").remove();
+        $("#pwd_check").append(
+            '<ul class="errorlist" id="error2"><li style="color: red; font-size: 20%">' +
+            'Two password inputs do not match.</li></ul>'
+        );
+        return false;
+    }
+    return true;
+}
+
+function userCheck() {
+    const username = $("#exampleInputUsername").val();
+    if (!username){
+        $("#error1").remove();
+        $("#usercheck").append(
+            '<ul class="errorlist" id="error1"><li style="color: red; font-size: 20%">' +
+            'Username can not be empty.</li></ul>'
+        );
+        return false;
+    }
+    else {
+        let flag = null;
+        $.ajax({
+            url: "https://petstore.swagger.io/v2/user/" + username,
+            type: "GET",
+            async: false,
+            cache: false,
+            success: function (data){
+                $("#error1").remove();
+                $("#usercheck").append(
+                    '<ul class="errorlist" id="error1"><li style="color: red; font-size: 20%">' +
+                    'This username has been used.</li></ul>'
+                );
+                flag = false;
+            },
+            error: function (error) {
+                console.log(error.responseJSON);
+                flag = true;
+            }
+        });
+        return flag;
+    }
+}
+
+$(function() {
+    $("#exampleInputUsername").focus(
+        function () {
+            $("#error1").remove();
+        }
+    )
+    $(".pwdcheck").focus(
+        function () {
+            $("#error2").remove();
+        }
+    )}
+);
